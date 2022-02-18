3640e45abbc84e20055b0c9b8f65b23b2f69e0aa
Ruiz Mendez Gerardo <gerardoruizmendez012@gmail.com>
2022-02-18 10:51:49 -0600

    Creando generador de frases

--- generaFrase.py
+++ generaFrase.py
@@ -1,20 +1,32 @@
+from pickle import TRUE
 import random, sqlite3
 
 conexion=sqlite3.connect("BBDD/frases")
 cursor=conexion.cursor()
 
 def genera():
-    cursor.execute("SELECT nombre FROM comida")
-    print(cursor.fetchall()[1][0])
-
-""""
-frase=""
-    if(random.randrange(1,3)==1):
+    tipo=random.randrange(1,3)
+    frase=""
+    if(tipo==1):
         cursor.execute("SELECT nombre FROM comida")
-        print(cursor.fetchall())
-        #cursor.execute("SELECT nombre FROM comida WHERE id=" +random.randrange(1,))
-        frase+=frase
-"""
+        tam=len(cursor.fetchall())
+        cursor.execute("SELECT nombre FROM comida WHERE id=" +str(random.randrange(1,tam+1)))
+        frase+=cursor.fetchall()[0][0] +" "
 
+        cursor.execute("SELECT nombre FROM conector")
+        tam=len(cursor.fetchall())
+        cursor.execute("SELECT nombre FROM conector WHERE id=" +str(random.randrange(1,tam+1)))
+        frase+=cursor.fetchall()[0][0] +" "
 
-genera()
+        cursor.execute("SELECT nombre FROM extra")
+        tam=len(cursor.fetchall())
+        cursor.execute("SELECT nombre FROM extra WHERE id=" +str(random.randrange(1,tam+1)))
+        frase+=cursor.fetchall()[0][0]
+
+    else:
+        cursor.execute("SELECT nombre FROM extra")
+        tam=len(cursor.fetchall())
+        cursor.execute("SELECT nombre FROM extra WHERE id=" +str(random.randrange(1,tam+1)))
+        frase+=cursor.fetchall()[0][0]
+
+    return frase
